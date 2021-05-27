e45640f17c4786b4d0849ac10f858282b55fc162
arybski <01132993@pw.edu.pl>
2021-05-27 12:01:45 +0200

    small changes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pickle
 import numpy as np
 from neural_network import NeuralNetwork
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+
 if __name__ == "__main__":
     # open data
     with open("./mnist.pkl", "rb") as f:
@@ -14,12 +17,15 @@ if __name__ == "__main__":
     print(train_data.shape)  # shape
     print(train_data[0])  # show data
 
+    print(np.max(train_data))
 
     train_labels = data["training_labels"]
 
     # just basic things
+    # normalization goes like this x =  (x-mean(x))/std(x)
+    train_data = train_data/255
+    print(train_data[0])
     n = NeuralNetwork(train_data[0].size, 10, 100)
-    n.stochastic_gradient(train_data[:100], train_labels[:100])
-
-    
-
+    ret = n.stochastic_gradient(train_data[:1000], train_labels[:1000])
+    plt.plot(np.linspace(0, 999, 1000)[:10], ret[:10])
+    plt.show()
--- neural_network.py
+++ neural_network.py
@@ -33,13 +33,15 @@ class NeuralNetwork:
         self.output_layer = None
         self.hidden_layer = None
         # TODO: Check how weights should be generated at first
-        self.weights_first = np.random.random([hidden_layer_size + 1, numbers_of_params + 1]) - 0.5
-        self.weights_second = np.random.random([number_of_labels, hidden_layer_size + 1]) - 0.5
+        self.weights_first = 2 * (np.random.random([hidden_layer_size + 1, numbers_of_params + 1]) - 0.5) * np.sqrt(
+            numbers_of_params)
+        self.weights_second = 2 * (np.random.random([number_of_labels, hidden_layer_size + 1]) - 0.5) * np.sqrt(
+            hidden_layer_size)
 
     def count_values_in_layers(self):
         self.hidden_layer = activation_function(np.matmul(self.weights_first, self.first_layer.T))
-        self.output_layer = np.matmul(self.weights_second, self.hidden_layer.T)
         self.hidden_layer[-1] = 1
+        self.output_layer = np.matmul(self.weights_second, self.hidden_layer.T)
 
     def set_value_in_first_layer(self, values: np.ndarray):
         self.first_layer = values
@@ -69,17 +71,22 @@ class NeuralNetwork:
         dq_dy1 = np.matmul((2 * (self.output_layer - expected_result)), self.weights_second)  # vector
         dq_ds = dq_dy1 * activation_function_derivative(np.matmul(self.weights_first, self.first_layer.T))  # vector
         return np.outer(dq_ds, self.first_layer)
+
     def stochastic_gradient(self, train_data: np.ndarray, train_expected: np.ndarray):
-        #TODO podobno to sie oplaca robic jako losowanie ze zwracaniem tych probek, ale kiedy to konczyc? Chyba dziala tak, ze my 'udajemy ze to losujemy'
+        # TODO podobno to sie oplaca robic jako losowanie ze zwracaniem tych probek, ale kiedy to konczyc? Chyba dziala tak, ze my 'udajemy ze to losujemy'
         # ale pewnosci nie mam.
-        beta = 0.2 # staly krok można zmienić
+        beta = 1  # staly krok można zmienić
+        temp = []
         for iter in range(train_data.shape[0]):
             self.set_value_in_first_layer(train_data[iter])
             self.count_values_in_layers()
-            self.weights_second -= beta*self.gradient_output_layer(int(train_expected[iter]))
+            self.weights_second -= beta * self.gradient_output_layer(int(train_expected[iter]))
             # pytanie: Jeśli dobrze rozumiem w metodzie wstecznej propagacji jest tak ze najpierw oblcizmy gradient
             # warstw ostatnich i potem gradienty ukrtych zalezy od tych wag dalszych, tylko przed czy po aktualizacji
-            self.weights_first -= beta*self.gradient_hidden_layers(int(train_expected[iter]))
+            self.weights_first -= beta * self.gradient_hidden_layers(int(train_expected[iter]))
+            temp.append(self.count_cost_function(int(train_expected[iter])))
+        return temp
+
     def classify(self, data):
         '''
         Do only after stochastic_gradient!!!
@@ -89,6 +96,7 @@ class NeuralNetwork:
         self.first_layer(data)
         self.count_values_in_layers()
         return np.argmax(self.output_layer)
+
 
 '''
 
